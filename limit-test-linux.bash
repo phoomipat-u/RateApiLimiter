7bc5aa9b965edf51b9382e3adb003d553fe07b1d
Phoomipat U <phoomipat.u@gmail.com>
2021-06-10 20:53:29 +0700

    feature/dockerfile - change port of script to 5001, move powershellscript to root

--- limit-test-linux.bash
+++ limit-test-linux.bash
@@ -1,6 +1,6 @@
 for i in {1..25}
 do
   printf "Call $i result is "
-  curl -s -o /dev/null -w "%{http_code}"  "http://localhost:5008/hotel/city?city=Bangkok"
+  curl -s -o /dev/null -w "%{http_code}"  "http://localhost:5001/hotel/city?city=Bangkok"
   printf "\n"
 done
